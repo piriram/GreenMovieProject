6928de1362055c1156941ea867d594c68de2a70b
piriram <pyoram25@gmail.com>
2025-07-31 19:21:57 +0900

    Chore: 폴더링 구조 세움

--- GreenMovieProject/Resource/SceneDelegate.swift
+++ GreenMovieProject/Resource/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let rootVC = CinemaViewController() 
+        let rootVC = NicknameCreateViewController()
         let nav = UINavigationController(rootViewController: rootVC)
         
         window = UIWindow(windowScene: windowScene)
